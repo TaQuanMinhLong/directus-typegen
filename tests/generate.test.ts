e6596c5cf9cab08ad49dd7250fa0352160af3bae
taquanminhlong <tqml2013@gmail.com>
2025-09-05 10:52:59 +0700

    CHORE: remove unused

--- tests/generate.test.ts
+++ tests/generate.test.ts
@@ -1,17 +1,17 @@
-// import { expect, test } from "bun:test";
-// import typescript from "typescript";
-// import generate from "~/typegen";
+import { expect, test } from "bun:test";
+import typescript from "typescript";
+import generate from "~/typegen";
 
-// test.skipIf(!process.env.TEST_GENERATE)("generate", async () => {
-//   const outFile = await generate({
-//     directusSdkImports: ["DirectusFile", "DirectusRole", "DirectusUser"],
-//   });
-//   expect(outFile).toBe(`${process.cwd()}/types.generated.ts`);
-//   const program = typescript.createProgram([outFile], {
-//     noEmit: true,
-//     strict: true,
-//   });
-//   const diagnostics = typescript.getPreEmitDiagnostics(program);
-//   expect(diagnostics.length).toBe(0);
-//   await Bun.file(outFile).unlink();
-// });
+test("generate", async () => {
+  const outFile = await generate({
+    directusSdkImports: ["DirectusFile", "DirectusRole", "DirectusUser"],
+  });
+  expect(outFile).toBe(`${process.cwd()}/types.generated.ts`);
+  const program = typescript.createProgram([outFile], {
+    noEmit: true,
+    strict: true,
+  });
+  const diagnostics = typescript.getPreEmitDiagnostics(program);
+  expect(diagnostics.length).toBe(0);
+  await Bun.file(outFile).unlink();
+});
